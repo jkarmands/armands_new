8ae45e8a4e93bcabfd2a7328b617b9a5d031eb1c
A <zukers.armands@gmail.com>
2020-12-09 20:07:59 +0200

    cipari

---
+++ Izvade_ievade/ievade.py
@@ -0,0 +1,15 @@
+print("sk1")
+sk1 = int(input())
+print  ("tavs sk1 ir {}".format(sk1))
+
+print("ak2")
+sk2 = int(input())
+print  ("tavs sk1 ir {}".format(sk2))
+
+
+
+#mnodulis
+print(sk1 % sk2) 
+
+
+print (sk1 // sk2)
