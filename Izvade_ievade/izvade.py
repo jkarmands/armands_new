a5b332af7e7589faba5a4bb39770bf688d52fc43
A <zukers.armands@gmail.com>
2020-12-09 19:44:26 +0200

    l2

--- Izvade_ievade/izvade.py
+++ Izvade_ievade/izvade.py
@@ -1,1 +1,23 @@
 print ("Sveika pasaule")
+##import('izvade2.py')
+
+ #Komentars - 
+#  dfssdfsd
+
+#  sdfsdfsdf
+#  sdfsd
+#  Falsedfsdfsdf
+#  sd
+#  fsfsdf
+
+vards = "Armands"
+vards2 = "Armands2"
+print(vards, vards2, )
+
+sk1 = 1
+sk2 = 2
+
+print (sk1 + sk2)
+print ('Esi sveicināts {}, tavs pirmais skaitlis {}'.format(vards, sk1, sk2))
+
+
