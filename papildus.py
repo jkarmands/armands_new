10c9eb979755fd379da7591ecb6a479087e17e10
jkarmands <75387346+jkarmands@users.noreply.github.com>
2021-02-26 18:46:19 +0200

    Add files via upload

---
+++ papildus.py
@@ -0,0 +1,24 @@
+#Python versija----------------------------------------------- 1 p
+import platform
+print("Mana Python versija ir " +platform.python_version())
+#-------------------------------------------------------------
+
+
+#dzejolītis -------------------------------------------------------------------------------------------- 1 p
+print("Mācies, kamēr gudrs tiec, \n Visu labi vērā liec.", "\nDzīve gudra, viltīga - \n Gudrība tur derīga!")
+#-------------------------------------------------------------
+
+#Apgriezts vārds-------------------------------- 2 p
+name = input()
+print(name[::-1])
+#-----------------------------------------------
+
+#Kauns šo punktu nepaņemts------------------------- 1 p
+print("Man viss izdodas!")
+#-------------------------------------------------
+
+#Pārbaudam vai cipars atrodas intervālā ---------- 2 p
+cipars = int(input())
+is_between = cipars in range(100, 1000)
+print(is_between)
+#------------------------------------------------- 
